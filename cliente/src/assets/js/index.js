9de25872c97692f4986bdb9d77df6e2273478c17
rgalled <rgalled2605@ieszaidinvergeles.org>
2025-11-30 17:00:33 +0100

    fixed left nav

--- cliente/src/assets/js/index.js
+++ cliente/src/assets/js/index.js
@@ -1,56 +1,59 @@
-document.querySelector(".button-cart").addEventListener("click", (e)=>{
-    let carrito = document.querySelector(".Carrito")
 
-    const style = window.getComputedStyle(carrito);
+addEventListener("DOMContentLoaded", (event) => {
+    document.querySelector(".button-cart").addEventListener("click", (e) => {
+        let carrito = document.querySelector(".Carrito")
 
-    if(style.display == "flex"){
-        carrito.style.display = 'none';
-    }else{
-        carrito.style.display = 'flex';
-    }
+        const style = window.getComputedStyle(carrito);
 
-    
-})
-
-
-document.querySelector("footer").addEventListener("click", (e)=>{
-    let ClientOrder = document.querySelector(".ClientOrderGestor")
-    console.log(ClientOrder);
-    const style = window.getComputedStyle(ClientOrder);
-
-    if(style.display == "block"){
-        ClientOrder.style.display = 'none';
-    }else{
-        ClientOrder.style.display = 'block';
-    }
-
-    
-})
+        if (style.display == "flex") {
+            carrito.style.display = 'none';
+        } else {
+            carrito.style.display = 'flex';
+        }
 
 
-document.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("close")){
-        if(e.target.parentElement.classList.contains("cross")){
-            let truecross = e.target.parentElement
-            if(truecross.parentElement.classList.contains("Carrito-header")){
-                
-                let carrito = document.querySelector(".Carrito")
-                carrito.style.display = 'none';
-            }
-            if(truecross.parentElement.classList.contains("ClientOrderHeader")){
-                let ClientOrder = document.querySelector(".ClientOrderGestor")
-                ClientOrder.style.display = 'none';
-            }
-        }else{
-            if(e.target.parentElement.classList.contains("Carrito-header")){
-                
-                let carrito = document.querySelector(".Carrito")
-                carrito.style.display = 'none';
-            }
-            if(e.target.parentElement.classList.contains("ClientOrderHeader")){
-                let ClientOrder = document.querySelector(".ClientOrderGestor")
-                ClientOrder.style.display = 'none';
+    })
+
+
+    document.querySelector("footer").addEventListener("click", (e) => {
+        let ClientOrder = document.querySelector(".ClientOrderGestor")
+        console.log(ClientOrder);
+        const style = window.getComputedStyle(ClientOrder);
+
+        if (style.display == "block") {
+            ClientOrder.style.display = 'none';
+        } else {
+            ClientOrder.style.display = 'block';
+        }
+
+
+    })
+
+
+    document.addEventListener("click", (e) => {
+        if (e.target.classList.contains("close")) {
+            if (e.target.parentElement.classList.contains("cross")) {
+                let truecross = e.target.parentElement
+                if (truecross.parentElement.classList.contains("Carrito-header")) {
+
+                    let carrito = document.querySelector(".Carrito")
+                    carrito.style.display = 'none';
+                }
+                if (truecross.parentElement.classList.contains("ClientOrderHeader")) {
+                    let ClientOrder = document.querySelector(".ClientOrderGestor")
+                    ClientOrder.style.display = 'none';
+                }
+            } else {
+                if (e.target.parentElement.classList.contains("Carrito-header")) {
+
+                    let carrito = document.querySelector(".Carrito")
+                    carrito.style.display = 'none';
+                }
+                if (e.target.parentElement.classList.contains("ClientOrderHeader")) {
+                    let ClientOrder = document.querySelector(".ClientOrderGestor")
+                    ClientOrder.style.display = 'none';
+                }
             }
         }
-    }
-});
+    });
+})
